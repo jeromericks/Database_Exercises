2c69f02b74f523fb126f4e7d32324a96c7d65fd0
jeromericks <jeromericks@gmail.com>
2015-11-20 15:57:27 -0600

    Changed name to album name in caption for the release dates

--- update_exercises.sql
+++ update_exercises.sql
@@ -8,11 +8,11 @@ SELECT sales AS 'New Updated Sales' FROM albums;
 ALTER TABLE albums
 MODIFY COLUMN release_date INT;
 
-SELECT name AS 'Name', release_date AS 'Albums released before 1980' FROM albums WHERE release_date < '1980';
+SELECT name AS 'Album names', release_date AS 'Albums released before 1980' FROM albums WHERE release_date < '1980';
 UPDATE albums
 SET release_date = release_date - 100
 WHERE release_date < '1980';
-SELECT name AS 'Name', release_date AS 'Updated albums' FROM albums WHERE release_date < '1980';
+SELECT name AS 'Album names', release_date AS 'Updated album release dates' FROM albums WHERE release_date < '1980';
 
 SELECT name AS 'Album''s by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums
