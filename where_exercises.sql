0bf10871d552205279af2be4ea9af1626f6ebc8a
jeromericks <jeromericks@gmail.com>
2015-11-23 13:18:43 -0600

    Completed where continued exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,20 +2,36 @@ USE employees;
 
 SELECT * 
 FROM employees
-WHERE first_name IN('Irena', 'Vidya', 'Maya');
+WHERE first_name IN('Irena', 'Vidya')
+OR first_name = 'Maya';
 
 SELECT * 
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE gender = 'M'
+	AND(
+		first_name IN('Irena', 'Vidya')
+		OR first_name = 'Maya'
+	);
 
 SELECT * 
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+SELECT * 
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+
+SELECT * 
+FROM employees
+WHERE birth_date LIKE '%12-25%' 
+	AND(
+		hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+	);
+
 
 SELECT *
 FROM employees
-WHERE birth_date LIKE '%12-25%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
