ebbe4c1c9a4f6355c58b127cda00585995715203
jeromericks <jeromericks@gmail.com>
2015-11-24 10:46:23 -0600

    Completed functions assignment

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -10,12 +10,13 @@ FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E';
 
-SELECT DATEDIFF(NOW(), '1990-01-01') 
+SELECT DATEDIFF(NOW(), hire_date) AS 'Days working at firm', first_name, last_name, hire_date
 FROM employees
 WHERE birth_date LIKE '%12-25%' 
 	AND(
 		hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-	);
+	)
+ORDER BY hire_date;
 
 SELECT DISTINCT first_name, last_name, COUNT(*) as Num
 FROM employees
