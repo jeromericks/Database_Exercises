0ba0b581e8af46eb413afb289970d6bf212fe80b
jeromericks <jeromericks@gmail.com>
2015-12-06 01:05:13 -0600

    Improved spacing on subquery exercise

--- subquery_exercise.sql
+++ subquery_exercise.sql
@@ -21,6 +21,7 @@ JOIN departments AS d
 ON d.dept_no = dm.dept_no
 WHERE e.gender = 'F' AND dm.to_date > NOW();
 
+
 SELECT CONCAT(first_name, ' ', last_name) AS full_name, gender
 FROM employees
 WHERE emp_no IN (
@@ -38,6 +39,7 @@ ON dm.emp_no = e.emp_no
 JOIN departments AS d
 ON d.dept_no = dm.dept_no
 WHERE e.gender = 'F' AND dm.to_date > NOW();
+
 
 SELECT dept_name
 FROM departments
